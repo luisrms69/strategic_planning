df6b4d8775c0b5834d2962fc07b2417b0b069175
Luis Montanaro <it@buzola.mx>
2022-11-19 20:05:53 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Strategic Planning
+
+Modulo de planeacion estrategica basada en OKRs y KPIs bajo plataforma ERPNEXT
+
+#### License
+
+MIT
---
+++ strategic_planning/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Strategic Planning",
+			"type": "module",
+			"label": _("Strategic Planning")
+		}
+	]
